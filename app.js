ce9a007047baaa7472f9802a67914fdee42eb701
Andrew Thomas <asthomas@Macintosh.local>
2018-01-13 17:16:35 -0500

    Modified js functions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const phrase = document.getElementById('phrase');
 const qwerty = document.getElementById('qwerty');
 const ul = document.querySelector('#phrase ul');
 const phrases = ['Hit the books', 'Go cold turkey', 'Rule of thumb', 'Raining cats and dogs', 'Cut to the chase'];
+const phraseArray = getRandomPhraseArray(phrases);
 const missed = 0;
 
 startButton.addEventListener('click', (e) => {
@@ -16,8 +17,38 @@ function getRandomPhraseArray(arr){
 }
 
 function addPhraseToDisplay(arr){
-  for (i = 0; i <= arr.length; i += 0) {
-    const li = document.createElement('arr[i]');
+  for (i = 0; i <= arr.length; i += 1) {
+    const li = document.createElement('li');
+    li.textContent = arr[i];
     ul.appendChild(li);
+    if (arr[i] != ' ') {
+      li.className = 'letter';
+    } else {
+      li.className = 'space';
+    }
   }
 }
+
+addPhraseToDisplay(phraseArray);
+
+function checkLetter(letter) {
+  const letters = document.querySelectorAll('.letter');
+  for (i = 0; i <= letters.length; i += 1) {
+    if (letters[i] === letter ) {
+      letters[i].className = 'show';
+      const match = letters[i];
+      return match;
+    } else {
+      return null;
+    }
+  }
+}
+
+qwerty.addEventListener('click', (e) => {
+  const clickedLetter = event.target;
+  if (clickedLetter.tagName === 'BUTTON') {
+    clickedLetter.className = 'chosen';
+    clickedLetter.disabled = true;
+    const letterFound = checkLetter(clickedLetter);
+  }
+});
